f971143fc53ebbc3a84d41c8c45f22bed07bc619
stivan622 <stivan.tr@gmail.com>
2018-02-20 16:58:33 +0900

    自動生成の設定を変更

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,10 @@ module QuasiCase
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+    end
   end
 end
