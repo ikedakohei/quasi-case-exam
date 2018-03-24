04f301f801d3392a969450eb35a2e6dc6ff23fcd
kohei <kouhik06@gmail.com>
2018-03-25 08:40:07 +0900

    Edit flash message of signup & login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+      # set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+      if @user.sign_in_count == 1
+        flash[:notice] = I18n.t 'devise.registrations.signed_up'
+      else
+        flash[:notice] = I18n.t 'devise.sessions.signed_in'
+      end
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
