bb2f4f279b88f771cd66bae2f62ccee9a0434806
kohei <kouhik06@gmail.com>
2018-03-25 05:28:31 +0900

    Add minor changes to css & devise.rb.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,10 +259,10 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   if Rails.env.production?
     config.omniauth :facebook, ENV["FACEBOOK_ID_PRODUCTION"], ENV["FACEBOOK_SECRET_PRODUCTION"]
-  elsif Rails.env.staging?
-    config.omniauth :facebook, ENV["FACEBOOK_ID_STAGING"], ENV["FACEBOOK_SECRET_STAGING"]
-  else
+  elsif Rails.env.development?
     config.omniauth :facebook, ENV["FACEBOOK_ID_DEVELOPMENT"], ENV["FACEBOOK_SECRET_DEVELOPMENT"]
+  else
+    config.omniauth :facebook, ENV["FACEBOOK_ID_STAGING"], ENV["FACEBOOK_SECRET_STAGING"]
   end
 
   # ==> Warden configuration
