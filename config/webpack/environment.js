27b1f49aee5a4574b455f171310d1e2f01a68db3
kohei <kouhik06@gmail.com>
2018-03-25 01:33:32 +0900

    Add fontawesome4.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
-environment.plugins.set('Provide', new webpack.ProvidePlugin({
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery',
   Popper: ['popper.js', 'default']
 }))
+
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader'
+});
 
 module.exports = environment
