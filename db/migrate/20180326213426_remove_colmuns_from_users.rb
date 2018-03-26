1566352dafadccab5803aa84d73a34a7ef0da2ba
kohei <kouhik06@gmail.com>
2018-03-27 07:04:32 +0900

    Delete unnecessary functions from Devise

---
+++ db/migrate/20180326213426_remove_colmuns_from_users.rb
@@ -0,0 +1,15 @@
+class RemoveColmunsFromUsers < ActiveRecord::Migration[5.1]
+  def up
+    remove_column :users, :email
+    remove_column :users, :encrypted_password
+    remove_column :users, :reset_password_token
+    remove_column :users, :reset_password_sent_at
+  end
+
+  def down
+    add_column :users, :email, :string, null: false, default: ""
+    add_column :users, :encrypted_password, :string, null: false, default: ""
+    add_column :users, :reset_password_token, :string
+    add_column :users, :reset_password_sent_at, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180324181531) do
+ActiveRecord::Schema.define(version: 20180326213426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
@@ -32,8 +28,6 @@ ActiveRecord::Schema.define(version: 20180324181531) do
     t.datetime "updated_at", null: false
     t.string "name", default: "", null: false
     t.string "image"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
