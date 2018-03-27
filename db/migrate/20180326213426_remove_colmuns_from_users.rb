7b5b4b4348ed2736a18c7bdb5793e7ef4eac5d32
kohei <kouhik06@gmail.com>
2018-03-27 20:17:26 +0900

    Fix migration file

--- db/migrate/20180326213426_remove_colmuns_from_users.rb
+++ db/migrate/20180326213426_remove_colmuns_from_users.rb
@@ -11,5 +11,7 @@ class RemoveColmunsFromUsers < ActiveRecord::Migration[5.1]
     add_column :users, :encrypted_password, :string, null: false, default: ""
     add_column :users, :reset_password_token, :string
     add_column :users, :reset_password_sent_at, :datetime
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
   end
 end
