33ae2112fccf2c77ad847bdbfb924d7341a68219
kohei <kouhik06@gmail.com>
2018-03-31 02:53:22 +0900

    Fix layout for flash message

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,8 +3,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # app/models/user.rbにあるメソッドを使用
     @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in_and_redirect @user, event: :authentication
-    flash[:success] = @user.sign_in_count == 1 ? (I18n.t 'devise.registrations.signed_up')
-                                               : (I18n.t 'devise.sessions.signed_in')
+    flash[:notice] = @user.sign_in_count == 1 ? (I18n.t 'devise.registrations.signed_up')
+                                              : (I18n.t 'devise.sessions.signed_in')
   end
 
   def failure
