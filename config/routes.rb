7422608c923ea570298d3b40a2a948bce95e51a7
kohei <kouhik06@gmail.com>
2018-03-31 02:51:43 +0900

    Fix logout function

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   devise_scope :user do
-    delete 'logout', to: 'devise/sessions#destroy'
+    get 'logout', to: 'devise/sessions#destroy'
   end
 end
