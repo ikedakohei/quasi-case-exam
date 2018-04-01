a3dbd239200bf070d378cbe155db680d9c8aefff
kohei <kouhik06@gmail.com>
2018-04-02 00:03:46 +0900

    Change update_attributes to update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
+    if @user.update(user_params)
       flash[:notice] = I18n.t 'devise.registrations.updated'
       redirect_to root_url
     else
