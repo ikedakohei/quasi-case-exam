3acd3f342ce143193c5f3ab629db7a973cc9d3e6
kohei <kouhik06@gmail.com>
2018-04-01 21:43:27 +0900

    Add image uploader function

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,13 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.name = auth.info.name
-      user.image = auth.info.image
+      user.sns_image = auth.info.image
     end
+  end
+
+  mount_uploader :image, ImageUploader
+
+  def image_file
+    self.image.present? ? image : sns_image
   end
 end
