dcf8d72317f255c880e3771356df16e3df7ea316
kohei <kouhik06@gmail.com>
2018-04-01 18:24:15 +0900

    Edit users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,24 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:notice] = I18n.t 'devise.registrations.updated'
+      redirect_to root_url
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    User.find(params[:id]).destroy
+    flash[:notice] = I18n.t 'devise.registrations.destroyed'
+    redirect_to root_url
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :image)
   end
 end
