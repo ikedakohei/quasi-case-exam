13aaa414eec9b4c6a71c8ec9d97e0d5cd448594c
kohei <kouhik06@gmail.com>
2018-05-22 07:23:18 +0900

    Fix N+1 queries

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,6 +3,6 @@ class Invitation < ApplicationRecord
   belongs_to :project
 
   def self.notification_pages(params)
-    all.includes(:user).order(created_at: :desc).page(params).per(5)
+    all.includes(:project).order(created_at: :desc).page(params).per(5)
   end
 end
