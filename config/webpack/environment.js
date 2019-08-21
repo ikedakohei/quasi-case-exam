b17b0f7851ac3243db697ba6f40cf484d14274e6
kohei <kouhik06@gmail.com>
2019-08-21 12:16:56 +0900

    Upgrade webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -7,8 +7,31 @@ environment.plugins.append('Provide', new webpack.ProvidePlugin({
   Popper: ['popper.js', 'default']
 }))
 
+// UglifyJsPlugin のインスタンスを見つけて置き換える
+environment.plugins = environment.plugins
+  .map(plug => {
+    if (plug instanceof webpack.optimize.UglifyJsPlugin) {
+      return new webpack.optimize.UglifyJsPlugin({
+        sourceMap: false,
+        parallel: true,
+        mangle: false,
+        uglifyOptions: {
+          mangle: false
+        },
+        compress: {
+          warnings: false
+        },
+        output: {
+          comments: false
+        }
+      });
+    }
+    return plug;
+  })
+
 environment.loaders.get('sass').use.splice(-1, 0, {
   loader: 'resolve-url-loader'
 });
 
+environment.devtool = 'eval';
 module.exports = environment
